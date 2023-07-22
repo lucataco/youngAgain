5b3578b85ca4d2677cbd851cc972290c4d6b899c
lucataco <lucataco@umd.edu>
2023-07-22 13:09:16 -0400

    Vercel Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import Script from 'next/script'
+import { Analytics } from '@vercel/analytics/react';
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -7,6 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Script defer data-domain="youngagain.vercel.app" src="https://data.lucata.co/js/script.js" />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
